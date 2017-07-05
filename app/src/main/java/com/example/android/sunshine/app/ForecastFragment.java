efe6a8ab6f9cf0c74c6553d58326a5449f6ceee5
journeyman1 <andrew.charanovsky@gmail.com>
2017-07-05 23:58:50 +0200

    add fetching code, refactoring

---
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -0,0 +1,106 @@
+package com.example.android.sunshine.app;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.android.sunshine.app.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForecastFragment extends Fragment {
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] weatherData = getResources().getStringArray(R.array.dummyStrings);
+
+        ArrayAdapter<String> data = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview, weatherData);
+
+        ListView view = (ListView) rootView.findViewById(R.id.list_forecast);
+        view.setAdapter(data);
+
+        return rootView;
+    }
+
+    private class FetchWeatherTask extends AsyncTask<String, Void, String> {
+
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        @Override
+        protected String doInBackground(String... strings) {
+            getData();
+
+            return null;
+        }
+
+        private String getData() {
+            HttpURLConnection conn = null;
+            BufferedReader reader = null;
+
+            String response = null;
+
+            try {
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?zip=31-436,pl&mode=json&cnt=5&units=metric&APPID=84e95b0fb91ecd4688c0195b299e9af9");
+
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.connect();
+
+                InputStream stream = conn.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (stream == null)
+                    return null;
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0)
+                    return null;
+
+                response = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                e.printStackTrace();
+                return null;
+            } finally {
+                if (conn != null) {
+                    conn.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return response;
+        }
+    }
+}
